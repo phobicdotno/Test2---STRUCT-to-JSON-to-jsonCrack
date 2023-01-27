5c2fd953349eda7f436af9cac3ac910661ae9646
phobicdotno <spamme.post@gmail.com>
2023-01-27 10:58:01 +0100

    Now the atribdisplay becomes a valye, to the next key, but som data is missing

--- textToJSON.js
+++ textToJSON.js
@@ -1,42 +1,35 @@
 function textToJSON(text) {
-        const lines = text.split('\n');
-        let json = {};
-        let currentStruct = '';
-        let attribute = '';
-        for (let line of lines) {
-            if (line.startsWith('TYPE')) {
-                currentStruct = line.split(' ')[1];
-                json[currentStruct] = {};
-            } else if (line.startsWith('STRUCT')) {
-                continue;
-            } else if (line.startsWith('END_STRUCT')) {
-                currentStruct = '';
-                attribute = '';
-            } else if (line.startsWith("{")) {
-                attribute = line;
+    const lines = text.split('\n');
+    let json = {};
+    let currentStruct = null;
+    let attribute = null;
+    for (let i = 0; i < lines.length; i++) {
+        let line = lines[i].trim();
+        if (line.startsWith("TYPE")) {
+            const parts = line.split(' ');
+            currentStruct = parts[1];
+            json[currentStruct] = {};
+        } else if (line.startsWith("{") && line.endsWith("}")) {
+            attribute = line.substring(1, line.length - 1);
+            attribute = attribute.replace(/[^a-zA-Z0-9\s]/g, "");
+            json[currentStruct]["atribdisplay"] = attribute;
+        } else if (line.endsWith(";")) {
+            const parts = line.split(':');
+            let key = parts[0].trim();
+            let value = parts[1].trim();
+            value = value.substring(0, value.length - 1);
+            if (json[currentStruct]["atribdisplay"]) {
+                json[currentStruct][key] = {};
+                json[currentStruct][key]['atribdisplay'] = json[currentStruct]["atribdisplay"];
+                json[currentStruct][key]['type'] = value;
+                delete json[currentStruct]["atribdisplay"];
             } else {
-                const parts = line.split(':');
-                if (parts.length > 1) {
-                    const key = parts[0].trim();
-                    let value = parts[1].trim();
-                    json[currentStruct][key] = {};
-                    if (attribute !== '') {
-                        json[currentStruct][key]['attribute'] = attribute.substring(1, attribute.length - 1);
-                        attribute = '';
-                    }
-                    if (value.indexOf(";") !== -1) {
-                        let temp = value.split(";");
-                        value = temp[0].trim();
-                        json[currentStruct][key]['type'] = value;
-                        if (temp[1].indexOf("//") !== -1) {
-                            let temp2 = temp[1].split("//");
-                            json[currentStruct][key]['comment'] = temp2[1].trim();
-                        }
-                    } else {
-                        json[currentStruct][key]['type'] = value;
-                    }
-                }
+                json[currentStruct][key] = {};
+                json[currentStruct][key]['type'] = value;
             }
+        } else if (line.startsWith("//")) {
+            json[currentStruct][key]['comment'] = line.substring(2).trim();
         }
-        return json;
     }
+    return json;
+}
